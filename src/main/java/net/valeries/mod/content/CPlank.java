fe543acdc933f2dca40ee95fb657bdaf077e1156
ClammyMantis488 <clammymantis488@gmail.com>
2020-02-23 18:07:37 -0800

    Added Stairs

--- src/main/java/net/valeries/mod/content/CPlank.java
+++ src/main/java/net/valeries/mod/content/CPlank.java
@@ -1,6 +1,7 @@
 package net.valeries.mod.content;
 
         import net.valeries.mod.blocks.Wood.CPlankBlock;
+        import net.valeries.mod.blocks.Wood.CPlankStairs;
 
 public class CPlank {
 
@@ -21,6 +22,23 @@ public class CPlank {
     public static CPlankBlock WHITE_PLANKS;
     public static CPlankBlock YELLOW_PLANKS;
 
+    public static CPlankStairs BLEACHED_PLANK_STAIRS;
+    public static CPlankStairs BLUE_PLANK_STAIRS;
+    public static CPlankStairs BROWN_PLANK_STAIRS;
+    public static CPlankStairs CYAN_PLANK_STAIRS;
+    public static CPlankStairs GRAY_PLANK_STAIRS;
+    public static CPlankStairs GREEN_PLANK_STAIRS;
+    public static CPlankStairs LIGHT_BLUE_PLANK_STAIRS;
+    public static CPlankStairs LIGHT_GRAY_PLANK_STAIRS;
+    public static CPlankStairs LIME_PLANK_STAIRS;
+    public static CPlankStairs MAGENTA_PLANK_STAIRS;
+    public static CPlankStairs ORANGE_PLANK_STAIRS;
+    public static CPlankStairs PINK_PLANK_STAIRS;
+    public static CPlankStairs PURPLE_PLANK_STAIRS;
+    public static CPlankStairs RED_PLANK_STAIRS;
+    public static CPlankStairs WHITE_PLANK_STAIRS;
+    public static CPlankStairs YELLOW_PLANK_STAIRS;
+
     public static void init() {
 
         BLEACHED_PLANKS = new CPlankBlock("bleached_planks", 2.0f, 15.0f);
@@ -39,6 +57,23 @@ public class CPlank {
         RED_PLANKS = new CPlankBlock("red_planks", 2.0f, 15.0f);
         WHITE_PLANKS = new CPlankBlock("white_planks", 2.0f, 15.0f);
         YELLOW_PLANKS = new CPlankBlock("yellow_planks", 2.0f, 15.0f);
+
+        BLEACHED_PLANK_STAIRS = new CPlankStairs(BLEACHED_PLANKS.getDefaultState(), "bleached_plank_stairs", 2.0f, 15.0f);
+        BLUE_PLANK_STAIRS = new CPlankStairs(BLUE_PLANKS.getDefaultState(),"blue_plank_stairs", 2.0f, 15.0f);
+        BROWN_PLANK_STAIRS = new CPlankStairs(BROWN_PLANKS.getDefaultState(),"brown_plank_stairs", 2.0f, 15.0f);
+        CYAN_PLANK_STAIRS = new CPlankStairs(CYAN_PLANKS.getDefaultState(),"cyan_plank_stairs", 2.0f, 15.0f);
+        GRAY_PLANK_STAIRS = new CPlankStairs(GRAY_PLANKS.getDefaultState(),"gray_plank_stairs", 2.0f, 15.0f);
+        GREEN_PLANK_STAIRS = new CPlankStairs(GREEN_PLANKS.getDefaultState(),"green_plank_stairs", 2.0f, 15.0f);
+        LIGHT_BLUE_PLANK_STAIRS = new CPlankStairs(LIGHT_BLUE_PLANKS.getDefaultState(),"light_blue_plank_stairs", 2.0f, 15.0f);
+        LIGHT_GRAY_PLANK_STAIRS = new CPlankStairs(LIGHT_GRAY_PLANKS.getDefaultState(),"light_gray_plank_stairs", 2.0f, 15.0f);
+        LIME_PLANK_STAIRS = new CPlankStairs(LIME_PLANKS.getDefaultState(),"lime_plank_stairs", 2.0f, 15.0f);
+        MAGENTA_PLANK_STAIRS = new CPlankStairs(MAGENTA_PLANKS.getDefaultState(),"magenta_plank_stairs", 2.0f, 15.0f);
+        ORANGE_PLANK_STAIRS = new CPlankStairs(ORANGE_PLANKS.getDefaultState(),"orange_plank_stairs", 2.0f, 15.0f);
+        PINK_PLANK_STAIRS = new CPlankStairs(PINK_PLANKS.getDefaultState(),"pink_plank_stairs", 2.0f, 15.0f);
+        PURPLE_PLANK_STAIRS = new CPlankStairs(PURPLE_PLANKS.getDefaultState(),"purple_plank_stairs", 2.0f, 15.0f);
+        RED_PLANK_STAIRS = new CPlankStairs(RED_PLANKS.getDefaultState(),"red_plank_stairs", 2.0f, 15.0f);
+        WHITE_PLANK_STAIRS = new CPlankStairs(WHITE_PLANKS.getDefaultState(),"white_plank_stairs", 2.0f, 15.0f);
+        YELLOW_PLANK_STAIRS = new CPlankStairs(YELLOW_PLANKS.getDefaultState(),"yellow_plank_stairs", 2.0f, 15.0f);
 
 
     }
