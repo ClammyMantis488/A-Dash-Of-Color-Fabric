0ae60852412b6efbddf06a4f51e0901c763789f9
vales0310 <61950196+vales0310@users.noreply.github.com>
2020-03-08 14:51:49 -0700

    added pink and blue slime blocks

--- src/main/java/net/adashofcolor/mod/content/CSlimeBlock.java
+++ src/main/java/net/adashofcolor/mod/content/CSlimeBlock.java
@@ -5,8 +5,12 @@ import net.adashofcolor.mod.blocks.Misc.CSlimeBlockBlock;
 public class CSlimeBlock {
 
     public static CSlimeBlockBlock BLEACHED_SLIME_BLOCK;
+    public static CSlimeBlockBlock PINK_SLIME_BLOCK;
+    public static CSlimeBlockBlock BLUE_SLIME_BLOCK;
 
     public static void init() {
         BLEACHED_SLIME_BLOCK = new CSlimeBlockBlock("bleached_slime_block", 2.0f, 15.0f);
+        PINK_SLIME_BLOCK = new CSlimeBlockBlock("pink_slime_block", 2.0f, 15.0f);
+        BLUE_SLIME_BLOCK = new CSlimeBlockBlock("blue_slime_block", 2.0f, 15.0f);
     }
 }
